25e0365bb8b3115b29a22b321d9bf55835248da6
bhimamalbhage18 <bhimamalbhage@hotmail.com>
2024-10-03 23:58:01 -0500

    formatting code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 function newItem() {
-  let inputValue = $('#input').val();
-  let li = $('<li></li>').text(inputValue);
+  let inputValue = $("#input").val();
+  let li = $("<li></li>").text(inputValue);
 
   if (inputValue === "") {
     alert("You must write something!");
   } else {
-    $('#list').append(li);
+    $("#list").append(li);
   }
 
-  li.on('dblclick', function() {
-    li.toggleClass('strike');
+  li.on("dblclick", function () {
+    li.toggleClass("strike");
   });
 
-  let crossOutButton = $('<crossOutButton></crossOutButton>').text('X');
+  let crossOutButton = $("<crossOutButton></crossOutButton>").text("X");
   li.append(crossOutButton);
 
-  crossOutButton.on('click', function() {
-    li.addClass('delete');
+  crossOutButton.on("click", function () {
+    li.addClass("delete");
   });
 
-  $('#list').sortable();
+  $("#list").sortable();
 }
